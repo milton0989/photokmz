29582e2fd6ebcf961601cc45a0529a531c055383
Milton Villar <milton.suscripciones@gmail.com>
2023-12-31 00:16:28 -0300

    refactorizacion del modelo

--- src/photokmz/model.py
+++ src/photokmz/model.py
@@ -10,28 +10,39 @@ from gpsexif import image_lon, image_lat
 
 class Model:
     
-    def __init__(self, list_img, compresion, destino, nombre_kmz):
+    def __init__(self, list_img, compresion, path_kmz):
         self.list_img = list_img      # lista de imagenes a procesar
         self.compresion = compresion  # variable para comprimimir imagen
-        self.destino = destino        # directorio de destino para el archivo
-        self.nombre_kmz = nombre_kmz  # nombre del archivo kmz
+        self.path_kmz = path_kmz      # ruta del archivo kmz
 
 
     def crear_kmz(self):
-        dir_temp = tempfile.TemporaryDirectory()  #dir=os.getcwd()creo un directorio temporal en la direccion dir=OPCIONAL (depende S.O.)
-        path_temp = Path(dir_temp.name) # tranformar str a path
         
-        dir_img = os.path.join(path_temp,"img") # Creo la ruta (str) para el directorio img
-        path_img = Path(dir_img)    # tranformo str a path
-        os.mkdir(path_img)  # Creo el directorio '/img' dentro del directorio temporal
-
-        self._copiar_img(path_img)        
         if self.compresion == True:
-            self._comprimir_imagenes(path_img)        
-        self._crear_kml(path_temp, path_img)
-        self._comprimir_directorio(path_temp)
+            dir_temp = tempfile.TemporaryDirectory()  #creo un directorio temporal en la direccion dir=OPCIONAL (depende S.O.)
+            path_temp = dir_temp.name
+            self._copiar_img(path_temp)   
+            self._comprimir_imagenes(path_temp)
+            self.list_img = [os.path.join(path_temp,file) for file in os.listdir(path_temp) ]
+            os.chdir(os.path.expanduser('~')) # cambio de directorio para no tener error de recursividad
+        
+        kml = simplekml.Kml()
+        for file in self.list_img:
+            file_name = os.path.basename(file)
+            try:
+                longitud = image_lon(file)
+                latitud = image_lat(file)
+            except:
+                longitud, latitud = (None, None)
+            if longitud: #Se añade al kml unicamente si tiene valor de Longitud.
+                point = kml.newpoint(name = file_name , coords = [(longitud,latitud)])
+                picpath = kml.addfile(file)
+                point.description = '<img style="max-width:500px;max-height:500px" src="' + picpath +'" />'
+                point.style.iconstyle.icon.href = 'http://maps.google.com/mapfiles/kml/shapes/placemark_circle.png'
 
-        os.chdir(self.destino) # establecer puntero en directorio destino para evitar error de recursividad
+        kml.savekmz(self.path_kmz, format = False)
+
+
         #input("Pulse enter para finalizar: ") # solo para probar
              
         mensaje = f"Se ha creado el KMZ con exito!!"
@@ -55,40 +66,7 @@ class Model:
             img_orig.save(imagen, exif=exif)
         print("se han comprimido las imagenes")
         
-    def _crear_kml (self, ruta_tmp, ruta_img_tmp):
-        os.chdir(ruta_tmp)  # me coloco en el directorio tmp
-        kml = simplekml.Kml()
 
-      
-        for file in os.listdir(ruta_img_tmp):            
-            longitud = image_lon(f"./img/{os.path.relpath(file)}")
-            latitud = image_lat(f"./img/{os.path.relpath(file)}")
-            punto = kml.newpoint(name=os.path.relpath(file),
-                                coords=[(longitud,latitud)],
-                                description=f"""<BR><img style="max-width:500px;max-height:500px" src="img/{os.path.relpath(file)}"></img><BR>""",)  #{file} lon, lat, optional height
-            punto.style.iconstyle.icon.href = 'http://maps.google.com/mapfiles/kml/shapes/placemark_circle.png'
-        
-        kml.save("imagenes.kml")
-        
-        print('se ha creado el kml')
-    
-            
-            
-             
-
-    def _comprimir_directorio(self, ruta_tmp):        
-
-        mi_zip = zipfile.ZipFile(os.path.join(self.destino,f'{self.nombre_kmz}.kmz'), 'w')
-
-        for folder, subfolders, files in os.walk(ruta_tmp):
-            for file in files:
-                mi_zip.write(os.path.join(folder, file), os.path.relpath(os.path.join(folder,file), ruta_tmp), compress_type = zipfile.ZIP_DEFLATED)
-
-        mi_zip.close()
-              
-        print('se ha comprimido en kmz')
-        
-        
     
 
 if __name__ == '__main__':
@@ -97,6 +75,6 @@ if __name__ == '__main__':
                     'C:/Users/joha_/Desktop/PruebaOpenFile/imagenes_test/IMG_20200729_143007828.jpg',
                         'C:/Users/joha_/Desktop/PruebaOpenFile/imagenes_test/IMG_20200729_143039395_HDR.jpg')   
 
-    instancia_modelo = Model(imagenes_prueba,True,os.getcwd(),'nombre' )
+    instancia_modelo = Model(imagenes_prueba,True,os.path.join(os.getcwd(),'prueba.kmz') )
     instancia_modelo.crear_kmz()
 
